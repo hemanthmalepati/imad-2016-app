60a298d514a6ad4252994a16a24cbf767cfe16eb
hemanthmalepati <mhemanthnaidu.skht@gmail.com>
2016-10-04 19:04:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
     
 element.innerHTML = 'New value';
+
+//Move the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginleft='100px';
+}
