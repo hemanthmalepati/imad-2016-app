8ca9ef69d8ad527e16440d7bb3e36c5115e630ef
hemanthmalepati <mhemanthnaidu.skht@gmail.com>
2016-10-23 11:25:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ submit.onclick = function() {
  //Make the request  
  var names = request.responseText;
  names = JSON.parse(names); 
- httprequest.open('GET', 'http://hemanthmalepati.imad.hasura-app.io/submit-name?name=' + name, true);
- httprequest.send(null); 
+ request.open('GET', 'http://hemanthmalepati.imad.hasura-app.io/submit-name?name=' + name, true);
+ request.send(null); 
    
 };
